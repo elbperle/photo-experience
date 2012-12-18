8c7516bbfa9fa4a765c7f60ba9ab1ec66e18eef5
Christopher Blum <christopher.blum@spellboy.com>
2012-12-18 16:41:06 +0100

    load recursively photo dirs and show them

--- routes/photos.js
+++ routes/photos.js
@@ -1,59 +1,40 @@
 var Step = require("step");
 
 exports.photos = function(req, res){
-    getDirectories("./public/images/photos/", function(directories) {
-        console.log(directories);
-        res.render('photos', { title: 'Photos', activeNavItem: "photos", photoThemes: directories});
+    tree("./public/images/photos", function(err, directory) {
+        console.log(directory.children);
+        res.render('photos', { title: 'Photos', activeNavItem: "photos", photoTree: directory.children, path: require("path") });
     });
 };
 
-function getDirectories(path, callback) {
-    Step(
-        function readDirectory() {
-            fs.readdir(path, this);
-        },
-        
-        function cleanDotEntries(err, entries) {
-            return stripDotFiles(entries);
-        },
-        
-        function getStats(err, entries) {
-            var group =  this.group();
-            this.entries = entries;
-            for (var i=0; i<entries.length; i++) {
-                fs.stat(path + entries[i], group());
-            }
-        },
-        
-        function filterDirectories(err, stats) {
-            console.log(this.entries)
-            this.directories = [];
-            for (var i=0; i<stats.length; i++) {
-                if (stats[i].isDirectory()) {
-                    this.directories.push(this.entries[i]);
-                }
-            }
-            return true;
-        },
-        
-        function readSubDirectories(err) {
-            var group = this.group();
-            for(var i=0; i < this.directories.length; i++) {
-                fs.readdir(path + this.directories[i], group());
-            }
-        },
-        
-        function filterEmptyDirectories(err, subDirsContents) {
-            var cleanDirs = [];
-            for (var i=0; i < subDirsContents.length; i++) {
-                if(stripDotFiles(subDirsContents[i]).length > 0) {
-                    cleanDirs.push(this.directories[i]);
-                }                
-            }
-            callback(cleanDirs);
+function tree(dir, done) {
+  var results = {
+        "path": dir,
+        "children": [],
+        "isDirectory": true
+      };
+  fs.readdir(dir, function(err, list) {
+    if (err) { return done(err); }
+    list = stripDotFiles(list);
+    var pending = list.length;
+    if (!pending) { return done(null, results); }
+    list.forEach(function(file) {
+      fs.stat(dir + '/' + file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          tree(dir + '/' + file, function(err, res) {
+            
+            results.children.push(res);
+            results.isDirectory = true;
+            if (!--pending){ done(null, results); }
+          });
+        } else {
+          results.children.push({"path": dir + "/" + file});
+          if (!--pending) { done(null, results); }
         }
-    );
-}
+      });
+    });
+  });
+};
 
 function getFiles(path, callback) {
     fs.readdir(path, callback);
