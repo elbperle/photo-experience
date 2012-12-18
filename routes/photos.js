d1534c318a93ec59ea867c22f3971d582a740fb3
Christopher Blum <christopher.blum@spellboy.com>
2012-12-18 12:14:39 +0100

    use step for asynchronous operation and cleanup directories

--- routes/photos.js
+++ routes/photos.js
@@ -1,30 +1,62 @@
+var Step = require("step");
+
 exports.photos = function(req, res){
     getDirectories("./public/images/photos/", function(directories) {
+        console.log(directories);
         res.render('photos', { title: 'Photos', activeNavItem: "photos", photoThemes: directories});
     });
 };
 
 function getDirectories(path, callback) {
-    fs.readdir(path, function(err, entries) {
-        var directories = [];
-        var entriesToWaitFor = entries.length;
-        for (var i=0; i<entries.length; i++) {
-            (function(entry) {
-                fs.stat(path + entry, function(err, stat) {
-                    if (stat.isDirectory()) {
-                        directories.push(entry);
-                    }
-                    if (--entriesToWaitFor === 0) {
-                        callback(stripDotFiles(directories));
-                    }
-                })
-            })(entries[i]);
+    Step(
+        function readDirectory() {
+            fs.readdir(path, this);
+        },
+        
+        function cleanDotEntries(err, entries) {
+            return stripDotFiles(entries);
+        },
+        
+        function getStats(err, entries) {
+            var group =  this.group();
+            this.entries = entries;
+            for (var i=0; i<entries.length; i++) {
+                fs.stat(path + entries[i], group());
+            }
+        },
+        
+        function filterDirectories(err, stats) {
+            console.log(this.entries)
+            this.directories = [];
+            for (var i=0; i<stats.length; i++) {
+                if (stats[i].isDirectory()) {
+                    this.directories.push(this.entries[i]);
+                }
+            }
+            return true;
+        },
+        
+        function readSubDirectories(err) {
+            var group = this.group();
+            for(var i=0; i < this.directories.length; i++) {
+                fs.readdir(path + this.directories[i], group());
+            }
+        },
+        
+        function filterEmptyDirectories(err, subDirsContents) {
+            var cleanDirs = [];
+            for (var i=0; i < subDirsContents.length; i++) {
+                if(stripDotFiles(subDirsContents[i]).length > 0) {
+                    cleanDirs.push(this.directories[i]);
+                }                
+            }
+            callback(cleanDirs);
         }
-    });
+    );
 }
 
 function getFiles(path, callback) {
-    fs.readdir(path, callback)
+    fs.readdir(path, callback);
 }
 
 function stripDotFiles(files) {
